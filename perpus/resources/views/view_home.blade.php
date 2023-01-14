1122b627f44a8639c2dca32f52b8e62d93d8a83c
Dandy Rovalliandi <dandyrovalliandi0404@gmail.com>
2023-01-14 23:55:09 +0700

    first commit

---
+++ perpus/resources/views/view_home.blade.php
@@ -0,0 +1,16 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+    <h3>Pengertian Perpustakaan Menurut Para Ahli</h3>
+    <p>
+    <h4>Perpustakaan</h4>
+        Menurut Prof. Sulistyo Basuki "Perpustakaan adalah sebuah ruangan, bagian sebuah Gedung ataupun gedung 
+        itu sendiri yang digunakan untuk menyimpan buku dan terbitan lainnya yang biasanya disimpan menurut tata 
+        susunan tertentu untuk digunakan pembaca, bukan untuk dijual. Dalam pengertian buku dan terbitan lainnya 
+        termasuk di dalamnya semua bahan cetak, buku, majalah, laporan, pamflet, prosiding, manuskrip (naskah), 
+        lembaran musik, berbagai karya musik, berbagai karya media audio visual seperti film, slid ( slide), kaset, 
+        piringan hitam, bentuk mikro seperti mikrofilm, mikrofis, dan mikro buram ( micro opaque ). Webster menyatakan 
+        bahwa perpustakaan merupakan kumpulan buku, manuskrip, dan bahan pustaka lainnya yang digunakan untuk 
+        keperluan studi `atau bacaan, kenyamanan, atau kesenangan."
+@endsection
